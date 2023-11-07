be5e9c4f2cd8c9671d58066ad296d66a5e097941
RodrigoKND <84539101+RodrigoKND@users.noreply.github.com>
2023-11-06 22:49:17 -0400

    creación de la tabla para registro de valores booleanos

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-/*
-    -- Tenemos que definir la logica booleana de dos expresiones
-    -- Mostrarle en una tabla los valores hechos con falso y verdadero
-*/
-
 const input = document.querySelector('input');
 const variables = document.querySelector('.variables');
 
 const resultBtn = document.querySelector('.resultBtn');
 const textWarning = document.querySelector('.warning');
 
+const table = document.querySelector('table');
+
 resultBtn.addEventListener('click', () => {
     const splitResult = input.value.trim().split('');
     detectWhiteSpaces(splitResult);
-    console.log(splitResult);
+    // console.log(splitResult);
 })
 
 const detectWhiteSpaces = (array) => {
-    for(let i =0; i< array.length; i++){
-        if(array[i] === ' '){
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === ' ') {
             textWarning.textContent = 'Error en el cálculo, no debe dejar espacios en blanco';
             return;
         }
@@ -25,20 +22,57 @@ const detectWhiteSpaces = (array) => {
     detectVariables(array);
 }
 
-const detectVariables = (arrayVariables)=>{
+const detectVariables = (arrayVariables) => {
     let count = 0;
-    arrayVariables.filter(value=>{
-        if(value.match(/[a-zA-Z]/g)){
+    let valuesVariables = [];
+    arrayVariables.filter(value => {
+        if (value.match(/[a-zA-Z]/g)) {
             count++;
-            console.log(value)
-            console.log(count)
+            valuesVariables.push(value);
         }
     })
-    if(!variables.value){
+
+    if (!variables.value) {
         textWarning.textContent = 'No debe dejar vacio ningún elemento';
+    } else textWarning.textContent = '';
+
+    if (count === Number(variables.value)) {
+        //obtener las variables del ejercicio
+        //Hay que ponerlo en la tabla igual
+        createVariablesTable(table, valuesVariables);
+        createBodyTablesVariables(table, count);
+    } else textWarning.textContent = 'El valor de las variables tiene que ser igual al de su ejercicio';
+}
+
+const createVariablesTable = (table, valuesVariables) => {
+    const tr = document.createElement('tr');
+    valuesVariables.map(value => {
+        const th = document.createElement('th');
+        th.textContent += value.toLocaleUpperCase();
+        tr.appendChild(th);
+    })
+
+    table.appendChild(tr);
+}
+
+const createBodyTablesVariables = (table, quantityVariables) => {
+    const tr = document.createElement('tr');
+    const powVariables = Math.pow(2, quantityVariables);
+    for (let i = 0; i < quantityVariables; i++) {
+        const td = document.createElement('td');
+        for (let j = 0; j < powVariables; j++) {
+            const input = document.createElement('input');
+            input.style.fontWeight = '600';
+            input.classList.add('form-control', 'contentTable', 'text-center', 'text-uppercase');
+            td.appendChild(input);
+            tr.appendChild(td);
+        }
     }
 
-    if(String(count) !== variables.value){
-        detectVariables(arrayVariables);
-    }
+    table.appendChild(tr);
 }
+/*
+    De acuerdo a la cantidad de variables se debe sacar la formula para decidir
+    cuantas columnas del cuerpo de la tabla se debe crear y que los td tengan dentro
+    un input o un contentEditable
+*/
